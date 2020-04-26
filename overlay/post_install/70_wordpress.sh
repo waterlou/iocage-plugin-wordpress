341c7993bd45cde5a14cb8c70882f5df4070b359
Water Lou <water@waterworld.com.hk>
2020-04-26 23:29:31 +0800

    update wordpress config script

--- overlay/post_install/70_wordpress.sh
+++ overlay/post_install/70_wordpress.sh
@@ -15,14 +15,41 @@ PASS=`sysrc -f ${CFG} -n mysql_pass`
 
 CFG=${WORDPRESS_PATH}/wp-config.php
 cp ${WORDPRESS_PATH}/wp-config-sample.php ${CFG}
-sed -i '' "/put your unique phrase here/d" ${CFG}
 sed -i '' -e "s/database_name_here/${DB}/g" ${CFG}
 sed -i '' -e "s/username_here/${USER}/g" ${CFG}
 sed -i '' -e "s/password_here/${PASS}/g" ${CFG}
 
-SECRETKEY=/root/.secretkey 
+SECRETKEY=/root/.secretkey
 curl https://api.wordpress.org/secret-key/1.1/salt/ > ${SECRETKEY}
-cat ${SECRETKEY} >> ${CFG}
+count=0
+while IFS= read -r line
+do
+  case $count in
+    0)
+      key="'AUTH_KEY'" ;;
+    1)
+      key="'SECURE_AUTH_KEY'" ;;
+    2)
+      key="'LOGGED_IN_KEY'" ;;
+    3)
+      key="'NONCE_KEY'" ;;
+    4)
+      key="'AUTH_SALT'" ;;
+    5)
+      key="'SECURE_AUTH_SALT" ;;
+    6)
+      key="'LOGGED_IN_SALT'" ;;
+    7)
+      key="'NONCE_SALT'" ;;
+    *)
+      key="____NOKEY____" ;;
+  esac
+  sed -i '' -e "/${key}/ c\\
+${line}" ${CFG}
+  count=$((count+1))
+  # echo "$line"
+done < ${SECRETKEY}
+
 rm ${SECRETKEY}
 
 chown -R www:www ${WORDPRESS_PATH}
